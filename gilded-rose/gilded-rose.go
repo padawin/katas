93238a575e9a4e7305b7a9271bad9bdc72f99937
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2021-07-09 09:43:51 +0200

    Rewrite UpdateQuality by grouping the specific cases together

--- gilded-rose/gilded-rose.go
+++ gilded-rose/gilded-rose.go
@@ -7,52 +7,44 @@ type Item struct {
 
 func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
+		item := items[i]
+		var qualityEvolution int
+		var sellInEvolution int
 
-		if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-			if items[i].quality > 0 {
-				if items[i].name != "Sulfuras, Hand of Ragnaros" {
-					items[i].quality = items[i].quality - 1
-				}
-			}
-		} else {
-			if items[i].quality < 50 {
-				items[i].quality = items[i].quality + 1
-				if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].sellIn < 11 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
-					if items[i].sellIn < 6 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
-				}
-			}
-		}
-
-		if items[i].name != "Sulfuras, Hand of Ragnaros" {
-			items[i].sellIn = items[i].sellIn - 1
-		}
-
-		if items[i].sellIn < 0 {
-			if items[i].name != "Aged Brie" {
-				if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].quality > 0 {
-						if items[i].name != "Sulfuras, Hand of Ragnaros" {
-							items[i].quality = items[i].quality - 1
-						}
-					}
-				} else {
-					items[i].quality = items[i].quality - items[i].quality
-				}
+		// Items specific cases
+		// TODO move out
+		if item.name == "Aged Brie" {
+			qualityEvolution = 1
+			sellInEvolution = -1
+		} else if item.name == "Sulfuras, Hand of Ragnaros" {
+			qualityEvolution = 0
+			sellInEvolution = 0
+		} else if item.name == "Backstage passes to a TAFKAL80ETC concert" {
+			if item.sellIn <= 0 {
+				qualityEvolution = -item.quality
+			} else if item.sellIn <= 5 {
+				qualityEvolution = 3
+			} else if item.sellIn <= 10 {
+				qualityEvolution = 2
 			} else {
-				if items[i].quality < 50 {
-					items[i].quality = items[i].quality + 1
-				}
+				qualityEvolution = 1
 			}
+			sellInEvolution = -1
+		} else {
+			// Generic case
+			qualityEvolution = -1
+			sellInEvolution = -1
 		}
-	}
 
+		if item.sellIn <= 0 {
+			qualityEvolution *= 2
+		}
+		item.quality += qualityEvolution
+		if qualityEvolution > 0 && item.quality > 50 {
+			item.quality = 50
+		} else if item.quality < 0 {
+			item.quality = 0
+		}
+		item.sellIn += sellInEvolution
+	}
 }
