40c6dd6819efa918f45d1b4bb100682f7d1a8c4e
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2021-07-13 18:28:29 +0200

    Implement CalculateScore

--- farkle.go
+++ farkle.go
@@ -2,10 +2,31 @@ package farkle
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 )
 
 const maxDice = 6
+
+// Score for numbers appearing 1 or 2 times
+const basePointsUnit = 0
+
+// straight == all dice have a different value
+const scoreStraight = 1200
+
+// Base for the score for numbers appearing 3 or more times
+const baseScoreThreeOfAKind = 100
+
+// Override score for numbers appearing 1 or 2 times
+var specialScoreUnit = map[int]int{
+	1: 100,
+	5: 50,
+}
+
+// Override score for numbers appearing 3 or more times
+var specialScoreThreeOfAKind = map[int]int{
+	1: 1000,
+}
 
 func RollNDice(n int) ([]int, error) {
 	res := []int{}
@@ -17,4 +38,38 @@ func RollNDice(n int) ([]int, error) {
 		n--
 	}
 	return res, nil
+}
+
+func CalculateScore(values []int) int {
+	// Group per values
+	countsPerValue := map[int]int{}
+	for _, value := range values {
+		countsPerValue[value]++
+	}
+	if len(countsPerValue) == maxDice {
+		return scoreStraight
+	}
+	score := 0
+	for value, count := range countsPerValue {
+		score += calculateValueScore(value, count)
+	}
+	return score
+}
+
+func calculateValueScore(value, count int) int {
+	score := 0
+	var base int
+	var found bool
+	if count >= 3 {
+		if base, found = specialScoreThreeOfAKind[value]; !found {
+			base = value * baseScoreThreeOfAKind
+		}
+		score += base * int(math.Pow(2, float64(count-3)))
+	} else {
+		if base, found = specialScoreUnit[value]; !found {
+			base = basePointsUnit
+		}
+		score += base * count
+	}
+	return score
 }
--- farkle_test.go
+++ farkle_test.go
@@ -24,3 +24,67 @@ func Test_RollNDiceReturnsErrorIfNIsNegative(t *testing.T) {
 	g.Expect(len(res)).To(Equal(0))
 	g.Expect(err).To(Equal(fmt.Errorf("the number of dice to roll must be positive")))
 }
+
+func Test_CalculateScoreReturns0For0values(t *testing.T) {
+	g := NewGomegaWithT(t)
+	res := CalculateScore([]int{})
+	g.Expect(res).To(Equal(0))
+}
+
+func Test_CalculateScoreForDifferentValues(t *testing.T) {
+	type testData struct {
+		play  []int
+		score int
+	}
+	fixtures := []testData{
+		{[]int{1}, 100},
+		{[]int{1, 1}, 200},
+		{[]int{1, 1, 1}, 1000},
+		{[]int{1, 1, 1, 1}, 2000},
+		{[]int{1, 1, 1, 1, 1}, 4000},
+		{[]int{1, 1, 1, 1, 1, 1}, 8000},
+		{[]int{2}, 0},
+		{[]int{2, 2}, 0},
+		{[]int{2, 2, 2}, 200},
+		{[]int{2, 2, 2, 2}, 400},
+		{[]int{2, 2, 2, 2, 2}, 800},
+		{[]int{2, 2, 2, 2, 2, 2}, 1600},
+		{[]int{3}, 0},
+		{[]int{3, 3}, 0},
+		{[]int{3, 3, 3}, 300},
+		{[]int{3, 3, 3, 3}, 600},
+		{[]int{3, 3, 3, 3, 3}, 1200},
+		{[]int{3, 3, 3, 3, 3, 3}, 2400},
+		{[]int{4}, 0},
+		{[]int{4, 4}, 0},
+		{[]int{4, 4, 4}, 400},
+		{[]int{4, 4, 4, 4}, 800},
+		{[]int{4, 4, 4, 4, 4}, 1600},
+		{[]int{4, 4, 4, 4, 4, 4}, 3200},
+		{[]int{5}, 50},
+		{[]int{5, 5}, 100},
+		{[]int{5, 5, 5}, 500},
+		{[]int{5, 5, 5, 5}, 1000},
+		{[]int{5, 5, 5, 5, 5}, 2000},
+		{[]int{5, 5, 5, 5, 5, 5}, 4000},
+		{[]int{6}, 0},
+		{[]int{6, 6}, 0},
+		{[]int{6, 6, 6}, 600},
+		{[]int{6, 6, 6, 6}, 1200},
+		{[]int{6, 6, 6, 6, 6}, 2400},
+		{[]int{6, 6, 6, 6, 6, 6}, 4800},
+		// combinations
+		{[]int{1, 5, 5}, 200},
+		{[]int{1, 1, 5}, 250},
+		{[]int{4, 4, 4, 1, 5}, 550},
+		// straight
+		{[]int{1, 2, 3, 4, 5, 6}, 1200},
+		{[]int{3, 1, 6, 4, 2, 5}, 1200},
+	}
+	for _, fixture := range fixtures {
+		score := CalculateScore(fixture.play)
+		if score != fixture.score {
+			t.Errorf("Score: With play %v, expected %d but got %d ", fixture.play, fixture.score, score)
+		}
+	}
+}
