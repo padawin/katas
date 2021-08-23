b22d2337dc92351a1349172dadcd4cc1cd29431e
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2021-08-23 17:17:43 +0200

    Move script logic in separated file

--- gilded-rose/gilded-rose.go
+++ gilded-rose/gilded-rose.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-
-	python "github.com/sbinet/go-python"
 )
 
 type Item struct {
@@ -71,38 +69,4 @@ func updateItemQualityAndSellIn(item *GildedRoseItem, qualityEvolution ItemQuali
 		item.quality = 0
 	}
 	item.sellIn += int(sellInEvolution)
-}
-
-func executeItemUpdateScript(item *GildedRoseItem, defaultQualityEvolution ItemQualityEvolution, defaultSellInEvolution ItemSellInEvolution) (ItemQualityEvolution, ItemSellInEvolution, error) {
-	python.Initialize()
-	defer python.Finalize()
-
-	itemUpdateModule := python.PyImport_ImportModule(item.updateScript)
-	if itemUpdateModule == nil {
-		return defaultQualityEvolution, defaultSellInEvolution, fmt.Errorf("Error importing module: %s", item.updateScript)
-	}
-
-	updateFunc := itemUpdateModule.GetAttrString("update")
-	if updateFunc == nil {
-		return defaultQualityEvolution, defaultSellInEvolution, fmt.Errorf("Error importing function `update`")
-	}
-
-	args := python.PyTuple_New(1)
-	brieDict := python.PyDict_New()
-	python.PyDict_SetItem(brieDict, python.PyString_FromString("name"), python.PyString_FromString(item.name))
-	python.PyDict_SetItem(brieDict, python.PyString_FromString("quality"), python.PyInt_FromLong(item.quality))
-	python.PyDict_SetItem(brieDict, python.PyString_FromString("sellIn"), python.PyInt_FromLong(item.sellIn))
-	python.PyTuple_SetItem(args, 0, brieDict)
-	res := updateFunc.CallObject(args)
-	if !python.PyTuple_Check(res) || python.PyTuple_Size(res) != 2 {
-		return defaultQualityEvolution, defaultSellInEvolution, fmt.Errorf("update must return a tuple of 2 elements")
-	}
-	qe := python.PyTuple_GetItem(res, 0)
-	se := python.PyTuple_GetItem(res, 1)
-	if !python.PyInt_Check(qe) {
-		return defaultQualityEvolution, defaultSellInEvolution, fmt.Errorf("the first returned element of update must be an int")
-	} else if !python.PyInt_Check(se) {
-		return defaultQualityEvolution, defaultSellInEvolution, fmt.Errorf("the second returned element of update must be an int")
-	}
-	return ItemQualityEvolution(python.PyInt_AsLong(qe)), ItemSellInEvolution(python.PyInt_AsLong(se)), nil
 }
